598e2e16beeaab92716a825a0895e81b99f1b7f5
Wan Bachtiar <sindbach@gmail.com>
2018-08-25 22:01:31 +1000

    Stopped fetching data if an error is encountered

--- utils/build_dataset.py
+++ utils/build_dataset.py
@@ -9,15 +9,19 @@ API_KEY = os.environ.get("OMDB_API")
 OMDB_URL = "http://www.omdbapi.com/?i=tt%s&plot=full&r=json&apikey=%s"
 
 def main():
-    fdata = open("./ml-latest-small/tagged_plots.json", 'w')
+    fdata = open("./data/extra_data.json", 'w')
     flink = open("./ml-latest-small/links.csv", 'r')
     for line in flink:
         if line.startswith("movieId"):
             continue
         mid, imdb_id, _ = line.strip().split(",")
         resp = requests.get(OMDB_URL % (imdb_id, API_KEY))
+        print(imdb_id)
         print(resp.text)
         resp_json = json.loads(resp.text)
+        if "Error" in resp_json:
+            print("Encountered an error.")
+            break
         json.dump(resp_json, fdata)
         fdata.write("\n")
     flink.close()
